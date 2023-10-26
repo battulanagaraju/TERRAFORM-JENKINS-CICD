a2d260c436eca041de919ac9c417a187733b5a9e
battulanagaraju <battula.nagaraj@gmail.com>
2023-10-26 17:45:41 +0530

    changed s3 name and key pair

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "nagaraju_jenkins"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "nagaraju_jenkins"
   }
 }
